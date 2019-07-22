20958b08f51127ecdd329d64f126cec6b79765b1
danimartinlombardo <daniel.martin@cabify.com>
2019-07-22 21:38:38 +0200

    Inclusion checks

--- rs_to_mysql.py
+++ rs_to_mysql.py
@@ -31,30 +31,3 @@ try:
 except pymysql.Error as e:
 	print(str(e))
 print ("Check 6")
-
-
-
-
-
-import psycopg2
-import pymysql
- 
-# Get data from Mysql
-mysql_table_name = 'some_table'
-mysql_conn = pymysql.connect(host=MYSQL_HOST, port=3306, user=MYSQL_USERNAME, password=MYSQL_PASSWORD, database=MYSQL_DATABASE)
-mysql_cur = mysql_conn.cursor()
-mysql_cur.execute('select * from %s;' % mysql_table_name)
-description = mysql_cur.description
-rows = mysql_cur.fetchall()
- 
-# Insert data into Redshift
-redshift_table_name = 'some_table_2'
-redshift_conn = psycopg2.connect(host=REDSHIFT_HOST, port=3306, user=REDSHIFT_USERNAME, password=REDSHIFT_PASSWORD, database=REDSHIFT
-_DATABASE)
-redshift_cur = redshift_conn.cursor()
-insert_template = 'insert into %s (%s) values %s;'
-column_names = ', '.join([x[0] for x in description])
-values = ', '.join(['(' + ','.join(map(str, x)) + ')' for x in rows])
- 
-redshift_cur.execute(insert_template % (redshift_table_name, column_names, values))
-
